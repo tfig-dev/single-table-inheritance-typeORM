b1d289bfda0c1c7ce9d111d0aff78df9a0bb99da
Tiago Figueiredo <tiagofr.figueiredo@gmail.com>
2024-04-07 20:40:59 +0100

    enable cors and api url

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors({ origin: '*' });
+
   await app.listen(3000);
 }
 bootstrap();
